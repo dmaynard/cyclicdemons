3042d0c0cc6c39d26f3161150b08e15c7dd774bf
dmaynard <davidsmaynard@gmail.com>
2026-06-01 22:56:53 -0700

    Add history of cyclic demons to About page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -21,6 +21,9 @@ export const About: React.FC<AboutProps> = ({ isOpen, onClose }) => {
                     <p>
                         This application is a Cellular Automaton built with React and Rust. It uses a custom WebAssembly engine to extract the dominant colors from any image using the Median Cut Quantization algorithm. Those colors then become states in a Cyclic Demon automaton where colors "eat" each other in a continuous cycle, propagating across the image canvas.
                     </p>
+                    <p style={{ marginTop: '15px' }}>
+                        A type of cellular automata called cyclic space was discovered by David Griffeath of the University of Wisconsin at Madison in 1990. Cyclic Space is described in <a href="https://www.amazon.com/Magic-Machine-Handbook-Computer-Sorcery/dp/0716721252/ref=sr_1_1?crid=2N7PKFJOVUI49&dib=eyJ2IjoiMSJ9.NTLdsrNDLEk1x3LeOBQyww.nWfU52fpvLMdRJsWOJv1BBOrNoiGSeZ0rMMVjCoi1Ns&dib_tag=se&keywords=the+magic+machine+a.k.+dewdney&qid=1729741595&sprefix=the+magic+machine+a.k.+dewdnet%2Caps%2C169&sr=8-1" target="_blank" rel="noopener noreferrer" style={{ color: '#3498db', textDecoration: 'none' }}>The Magic Machine A Handbook of Computer Sorcery</a> by A.K. Dewdney.
+                    </p>
                     
                     <h3 style={{ marginTop: '15px', fontSize: '1em', color: '#ccc' }}>Features</h3>
                     <ul style={{ paddingLeft: '20px' }}>
